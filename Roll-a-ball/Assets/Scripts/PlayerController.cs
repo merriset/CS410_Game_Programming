2fe917530f791c239d4295de0ccd13bace5630f5
Peter N <peterNelson1@proton.me>
2023-04-10 21:26:29 -0700

    finished game with added jumping

--- Roll-a-ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -2,18 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     private Rigidbody rb;
     private float movementX;
     private float movementY;
+    private float movementZ = 0;
+    private int count;
+    private int jumpCount;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        jumpCount = 0;
+        setCountText();
+        winTextObject.SetActive(false);
     }
 
     void OnMove(InputValue movementValue)
@@ -23,19 +33,53 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    void OnJump(InputValue movementValue)
+    {
+        if(jumpCount < 1) 
+        {
+            rb.AddForce(Vector3.up * 5f, ForceMode.Impulse);
+            jumpCount++;  
+        }
+        
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 8)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         
         rb.AddForce(movement * speed);
+
+        if(isGrounded())
+        {
+            jumpCount = 0;
+        }
     }
 
     private void OnTriggerEnter(Collider other) 
     {
         if(other.gameObject.CompareTag("PickUp"))
         {
-            other.gameObject.SetActive(false);   
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();  
+        }   
+    }
+
+    private bool isGrounded()
+    {
+        if(transform.position.y < 0.6)
+        {
+            return true;
         }
-        
+        return false;
     }
 }
